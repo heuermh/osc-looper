cbcc50612301d759164edf094ff7ae41885c0571
U-HPI-T400\heuermh <heuermh@acm.org>
2010-12-08 20:44:46 -0600

    add missing class files to build script

--- build-library.sh
+++ build-library.sh
@@ -7,6 +7,6 @@ cp README osclooper
 cp -R src osclooper
 cp -R examples osclooper
 cd src
-javac -classpath "../lib/oscP5.jar" osclooper/OSCLooper.java
-jar cvf ../osclooper/library/osclooper.jar osclooper/OscLooper.class
+javac -classpath "../lib/oscP5.jar" osclooper/*.java
+jar cvf ../osclooper/library/osclooper.jar osclooper/*.class
 cd ..
